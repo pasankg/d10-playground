3a6c09a0fffcde3131c6367fb8490279d3a4be11
Pasan <pasankg@gmail.com>
2024-04-17 21:41:42 +1000

    Custom login menu link module

---
+++ web/modules/custom/playground_custom_login/src/Plugin/Menu/CustomLoginLogoutMenuLink.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\playground_custom_login\Plugin\Menu;
+
+use Drupal\Core\Menu\StaticMenuLinkOverridesInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\Plugin\Menu\LoginLogoutMenuLink;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Extension of menu link that shows "Sign in" or "Sign out" as appropriate.
+ */
+class CustomLoginLogoutMenuLink extends LoginLogoutMenuLink {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new LoginLogoutMenuLink.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Menu\StaticMenuLinkOverridesInterface $static_override
+   *   The static override storage.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StaticMenuLinkOverridesInterface $static_override, AccountInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $static_override, $current_user);
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('menu_link.static.overrides'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    if ($this->currentUser->isAuthenticated()) {
+      return $this->t('Log out');
+    }
+    else {
+      return $this->t('Sign in');
+    }
+  }
+
+}
